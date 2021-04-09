2a6ab7a373209a0472093048fc8df6893e6e5308
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-09 18:54:54 +0200

    add review controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     member do
       get :proprio
     end
+    resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_160955) do
+ActiveRecord::Schema.define(version: 2021_04_09_164712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 2021_04_09_160955) do
     t.string "proprio"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "flat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_reviews_on_flat_id"
+  end
+
+  add_foreign_key "reviews", "flats"
 end
