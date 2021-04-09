226ec69ad5048fd623886afe56a4a9e40742b531
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-09 18:09:11 +0200

    enc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'flats#index'
-  resources :flat, only: [:index]
+  resources :flats, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    collection do
+      get :top
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
